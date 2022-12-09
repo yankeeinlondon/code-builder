c6c682fc823e405086b84b966609fadf72001294
Ken Snyder <ken@ken.net>
2022-12-09 10:45:49 -0800

    chore: adjust vitest config and remove types from "exports"

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,14 +6,7 @@ import code from "./src/index";
 
 // used for testing, library code uses TSUP to build exports
 export default defineConfig({
-  test: {
-    dir: "test",
-    exclude: ["**/*.spec.ts"],
-    environment: "happy-dom",
-    coverage: {
-      reporter: ["json", "html"]
-    }
-  },
+
   plugins: [
     Markdown({
       builders: [ code() ],
